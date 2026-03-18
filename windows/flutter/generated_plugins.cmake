932488a60fbd983944c7142bdc55128f74440855
박 종천 <parkjc@equal.co.kr>
2026-03-18 14:23:32 +0900

    refactor: 프로젝트 최적화 - 미사용 패키지 7개 제거, 미참조 파일 삭제, 에셋 정리
    - 패키지 제거: dio, geolocator, riverpod_annotation, cupertino_icons, qr_flutter, share_plus, screenshot
    - dependency_overrides 7개 전체 제거
    - 미참조 provider 파일 3개 삭제 (desktop_icons, regional_folders, window_manager)
    - 불필요 테스트 2개 삭제 (widget_test, capture_icon_test)
    - 중복/임시 에셋 삭제 (cQqdx.png, images/ 폴더)
    - 개발 로그 파일 삭제 (audit_log, error_log, build_output)
    - .gitignore 보강 (pubspec.lock, *.ipa, *.apk 등)
    - 총 26개 transitive dependency 제거됨

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,10 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   firebase_auth
   firebase_core
-  geolocator_windows
   local_auth_windows
-  share_plus
-  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
